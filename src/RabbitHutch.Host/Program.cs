bc90aa1f0e943619bf352dc46b4895073cb640a6
Jacob Cunningham <jacobc@blinds.com>
2017-07-25 19:59:10 -0500

    basic sender and receiver apps

--- src/RabbitHutch.Host/Program.cs
+++ src/RabbitHutch.Host/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace RabbitHutch.Host
 {
@@ -10,6 +9,33 @@ namespace RabbitHutch.Host
     {
         static void Main(string[] args)
         {
+            var factory = new ConnectionFactory() {HostName = "localhost"};
+            using (var conn = factory.CreateConnection())
+            using (var channel = conn.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: "hello",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine($"Received {message}");
+                };
+
+                channel.BasicConsume(
+                    queue: "hello",
+                    autoAck: true,
+                    consumer: consumer);
+
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadLine();
+            }
         }
     }
 }
---
+++ src/RabbitHutch.TestHarness/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace RabbitHutch.TestHarness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using (var conn = factory.CreateConnection())
+            using (var channel = conn.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: "hello",
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                var message = "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(
+                    exchange: "",
+                    routingKey: "hello",
+                    basicProperties: null,
+                    body: body);
+                Console.WriteLine($"[x] Sent {message}");
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+        }
+    }
+}
