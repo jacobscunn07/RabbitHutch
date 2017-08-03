7795adbe656cf1b3aa2f41efe1aac0552501d45c
Jacob Cunningham <jacob.cunningham@live.com>
2017-08-02 23:24:28 -0500

    wire up structuremap webapi2

--- src/RabbitHutch.Web/Controllers/SearchController.cs
+++ src/RabbitHutch.Web/Controllers/SearchController.cs
@@ -4,14 +4,17 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using MediatR;
 
 namespace RabbitHutch.Web.Controllers
 {
     public class SearchController : ApiController
     {
-        public SearchController()
+        private readonly IMediator _mediator;
+
+        public SearchController(IMediator mediator)
         {
-            
+            _mediator = mediator;
         }
 
         public string Index()
