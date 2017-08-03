55759696f08520a07cfd0dd0cf6e28605fc5247e
Jacob Cunningham <jacob.cunningham@live.com>
2017-08-02 23:21:09 -0500

    add webapi2

--- src/RabbitHutch.Web/App_Start/RouteConfig.cs
+++ src/RabbitHutch.Web/App_Start/RouteConfig.cs
@@ -13,11 +13,15 @@ namespace RabbitHutch.Web
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //            routes.MapRoute(
+            //                name: "Default",
+            //                url: "{controller}/{action}/{id}",
+            //                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                "ReactApp",
+                "{*url}",
+                new { controller = "Home", action = "Index" }
+        );
         }
     }
 }
