3bcaec022cccf781216f3407327a325fd1bf587d
Jacob Cunningham <jacob.cunningham@live.com>
2017-08-09 21:37:13 -0500

    add empty controller for replaying a message

---
+++ src/RabbitHutch.Web/Controllers/ReplayController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+using MediatR;
+
+namespace RabbitHutch.Web.Controllers
+{
+    public class ReplayController : ApiController
+    {
+        private readonly IMediator _mediator;
+
+        public ReplayController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public bool Post(long docId)
+        {
+            return true;
+        }
+    }
+}
