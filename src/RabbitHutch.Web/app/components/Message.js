ab96ddcae6c2f82c75d719d3ff3af72d98fb0b6f
Jacob Cunningham <jacob.cunningham@live.com>
2017-08-09 23:14:29 -0500

    refactor message display to not use horizontal forms

--- src/RabbitHutch.Web/app/components/Message.js
+++ src/RabbitHutch.Web/app/components/Message.js
@@ -2,45 +2,27 @@ import React, { PropTypes } from 'react';
 
 const Message = props => (
   <div>
-    <div className="field is-horizontal">
-      <div className="field-label is-normal">
-        <label htmlFor="a" className="label">Service Bus</label>
-      </div>
-      <div className="field-body">
-        <div className="field">
-          <div className="control">
-            <input className="input" type="text" placeholder="Normal sized input" value={props.message.serviceBusTechnology} disabled />
-          </div>
-        </div>
+    <div className="field">
+      <label htmlFor="a" className="label">Service Bus</label>
+      <div className="control">
+        <input className="input" type="text" placeholder="Normal sized input" value={props.message.serviceBusTechnology} disabled />
       </div>
     </div>
 
-    <div className="field is-horizontal">
-      <div className="field-label is-normal">
-        <label htmlFor="a" className="label">Body</label>
-      </div>
-      <div className="field-body">
-        <div className="field">
-          <div className="control">
-            <textarea className="textarea" type="text" placeholder="Disabled textarea" value={props.message.body} disabled />
-          </div>
-        </div>
+    <div className="field">
+      <label htmlFor="a" className="label">Body</label>
+      <div className="control">
+        <textarea className="textarea" type="text" placeholder="Disabled textarea" value={props.message.body} disabled />
       </div>
     </div>
 
     {
       props.message.headers &&
       props.message.headers.map(header => (
-        <div className="field is-horizontal" key={header.key}>
-          <div className="field-label is-normal">
-            <label htmlFor="a" className="label">{header.key}</label>
-          </div>
-          <div className="field-body">
-            <div className="field">
-              <div className="control">
-                <input className="input" type="text" placeholder="Normal sized input" value={header.value} disabled />
-              </div>
-            </div>
+        <div className="field" key={header.key}>
+          <label htmlFor="a" className="label">{header.key}</label>
+          <div className="control">
+            <input className="input" type="text" placeholder="Normal sized input" value={header.value} disabled />
           </div>
         </div>
       ))
