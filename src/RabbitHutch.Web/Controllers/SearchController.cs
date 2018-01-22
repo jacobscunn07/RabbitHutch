71515b181e084f1116f5331041e6f514fcfc0d4d
Jacob Cunningham <jacob.cunningham@headspring.com>
2018-01-21 21:42:32 -0600

    display just class name on message list

--- src/RabbitHutch.Web/Controllers/SearchController.cs
+++ src/RabbitHutch.Web/Controllers/SearchController.cs
@@ -52,7 +52,7 @@ namespace RabbitHutch.Web.Controllers
                 Body = document.Body,
                 ProcessedEndpoint = parser.IsError ? parser.FailedQueue : parser.ProcessingEndPoint,
                 OriginatingEndpoint = parser.OriginatingEndPoint,
-                ClassType = parser.MessageTypes.Split(' ').Select(x => x.Remove(x.Length-1)).FirstOrDefault(),
+                ClassType = parser.MessageTypes.Split(' ').Select(x => x.Remove(x.Length-1)).FirstOrDefault().Split('.').LastOrDefault(),
                 ProcessedDateTime = document.ProcessedDateTime
             };
 
