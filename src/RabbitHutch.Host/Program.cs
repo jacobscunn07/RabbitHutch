37b3654dc02494153a7f22e0a12ab6528251783c
Jacob Cunningham <jacob.cunningham@live.com>
2018-06-14 22:22:48 -0500

    Host project upgraded to net471 and compiling

--- src/RabbitHutch.Host/Program.cs
+++ src/RabbitHutch.Host/Program.cs
@@ -1,6 +1,7 @@
-﻿using RabbitHutch.Application.Interfaces;
-using StructureMap;
-using Topshelf;
+﻿//using RabbitHutch.Application.Interfaces;
+//using StructureMap;
+//using Topshelf;
+using System;
 
 namespace RabbitHutch.Host
 {
@@ -8,23 +9,25 @@ namespace RabbitHutch.Host
     {
         static void Main(string[] args)
         {
-            var container = Container.For<RabbitHutchRegistry>();
+            //var container = Container.For<RabbitHutchRegistry>();
 
-            HostFactory.Run(x =>
-            {
-                x.Service<IHost>(s =>
-                {
-                    s.ConstructUsing(name => container.GetInstance<Application.Host>());
-                    s.WhenStarted(host => host.Start());
-                    s.WhenStopped(host => host.Stop());
-                });
-                x.RunAsLocalSystem();
+            //HostFactory.Run(x =>
+            //{
+            //    x.Service<IHost>(s =>
+            //    {
+            //        s.ConstructUsing(name => container.GetInstance<Application.Host>());
+            //        s.WhenStarted(host => host.Start());
+            //        s.WhenStopped(host => host.Stop());
+            //    });
+            //    x.RunAsLocalSystem();
 
-                x.SetDescription("Scrapes messages from Rabbit Audit and Error Queues.");
-                x.SetDisplayName("RabbitHutch Host");
-                x.SetServiceName("RabbitHutch.Host");
-                x.StartAutomatically();
-            });
+            //    x.SetDescription("Scrapes messages from Rabbit Audit and Error Queues.");
+            //    x.SetDisplayName("RabbitHutch Host");
+            //    x.SetServiceName("RabbitHutch.Host");
+            //    x.StartAutomatically();
+            //});
+            Console.WriteLine("Hey");
+            Console.ReadLine();
         }
     }
 }
--- src/RabbitHutch.Host/RabbitHutchRegistry.cs
+++ src/RabbitHutch.Host/RabbitHutchRegistry.cs
@@ -1,39 +1,39 @@
-﻿using MediatR;
-using RabbitHutch.DataAccess;
-using RabbitHutch.DataAccess.Raven;
-using Raven.Client;
-using Raven.Client.Document;
-using StructureMap;
+﻿//using MediatR;
+//using RabbitHutch.DataAccess;
+//using RabbitHutch.DataAccess.Raven;
+//using Raven.Client;
+//using Raven.Client.Document;
+//using StructureMap;
 
-namespace RabbitHutch.Host
-{
-    public class RabbitHutchRegistry : Registry
-    {
-        public RabbitHutchRegistry()
-        {
-            Scan(scan =>
-            {
-//                scan.TheCallingAssembly();
-                scan.AssemblyContainingType<RabbitHutch.Application.CommandHandlers.DocumentSearchQuery>();
-                scan.WithDefaultConventions();
-                scan.ConnectImplementationsToTypesClosing(typeof(IRequestHandler<>));
-                scan.ConnectImplementationsToTypesClosing(typeof(IRequestHandler<,>));
-                scan.ConnectImplementationsToTypesClosing(typeof(IAsyncRequestHandler<>));
-                scan.ConnectImplementationsToTypesClosing(typeof(IAsyncRequestHandler<,>));
-                scan.ConnectImplementationsToTypesClosing(typeof(INotificationHandler<>));
-                scan.ConnectImplementationsToTypesClosing(typeof(IAsyncNotificationHandler<>));
-            });
-            For<SingleInstanceFactory>().Use<SingleInstanceFactory>(ctx => t => ctx.GetInstance(t));
-            For<MultiInstanceFactory>().Use<MultiInstanceFactory>(ctx => t => ctx.GetAllInstances(t));
-            For<IMediator>().Use<Mediator>();
-            For<IDocumentStore>()
-              .Singleton()
-              .Use(new DocumentStore
-              {
-                  Url = "http://localhost:8080/",
-                  DefaultDatabase = "RabbitHutch"
-              }.Initialize());
-            For<IDatabase>().Use<RavenDatabase>();
-        }
-    }
-}
+//namespace RabbitHutch.Host
+//{
+//    public class RabbitHutchRegistry : Registry
+//    {
+//        public RabbitHutchRegistry()
+//        {
+//            Scan(scan =>
+//            {
+////                scan.TheCallingAssembly();
+//                scan.AssemblyContainingType<RabbitHutch.Application.CommandHandlers.DocumentSearchQuery>();
+//                scan.WithDefaultConventions();
+//                scan.ConnectImplementationsToTypesClosing(typeof(IRequestHandler<>));
+//                scan.ConnectImplementationsToTypesClosing(typeof(IRequestHandler<,>));
+//                scan.ConnectImplementationsToTypesClosing(typeof(IAsyncRequestHandler<>));
+//                scan.ConnectImplementationsToTypesClosing(typeof(IAsyncRequestHandler<,>));
+//                scan.ConnectImplementationsToTypesClosing(typeof(INotificationHandler<>));
+//                scan.ConnectImplementationsToTypesClosing(typeof(IAsyncNotificationHandler<>));
+//            });
+//            For<SingleInstanceFactory>().Use<SingleInstanceFactory>(ctx => t => ctx.GetInstance(t));
+//            For<MultiInstanceFactory>().Use<MultiInstanceFactory>(ctx => t => ctx.GetAllInstances(t));
+//            For<IMediator>().Use<Mediator>();
+//            For<IDocumentStore>()
+//              .Singleton()
+//              .Use(new DocumentStore
+//              {
+//                  Url = "http://localhost:8080/",
+//                  DefaultDatabase = "RabbitHutch"
+//              }.Initialize());
+//            For<IDatabase>().Use<RavenDatabase>();
+//        }
+//    }
+//}
